43c145b183168ac3f50a92c1582bde79a1417739
lucasfreitas25 <lucasfreitas25001@gmail.com>
2025-02-19 12:45:19 -0400

    Arrumar box em baixo da imagem

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,134 +1,144 @@
-import { Box, Container, Divider, Grid2, Icon, IconButton, ImageList, ImageListItem, ImageListItemBar, List, ListItem,  ListSubheader, Tab, Tabs, Typography, styled } from "@mui/material";
+import { Box, Container, Divider, Grid2, Icon, IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader, Tab, Tabs, Typography, styled } from "@mui/material";
 import React from "react";
-import StyleButton from "../../components/StyleButton/StyleButton";
 import ArchitectureIcon from '@mui/icons-material/Architecture';
 import CodeIcon from '@mui/icons-material/Code';
 import AddchartIcon from '@mui/icons-material/Addchart';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 const itemDev = [
-    {
-      img: 'src\\assets\\images\\crud .png',
-      title: 'CRUD',
-      author: '@bkristastucchio',
-      rows: 2,
-      cols: 2,
-      featured: true,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'SITE TROJAN',
-      author: '@rollelflex_graphy726',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Contador de TAGS',
-      author: '@helloimnik',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      title: 'LinkTree Custom',
-      author: '@nolanissac',
-      cols: 2,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-      title: 'FreitulitosAPP',
-      author: '@hjrc33',
-      cols: 2,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-      title: 'FirstAPI',
-      author: '@arwinneil',
-      rows: 2,
-      cols: 2,
-      featured: true,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-      title: 'Meu portfolio',
-      author: '@tjdragotta',
-    },
+  {
+    img: 'src\\assets\\images\\imagem portfolio.png',
+    title: 'Meu portfolio',
+    author: ['logo js.svg', 'logo react.png'],
+  },
+  {
+    img: 'src\\assets\\images\\crud .png',
+    title: 'CRUD',
+    author: ['logo java.png'],
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: 'src\\assets\\images\\Sistema inteligente.png',
+    title: 'SISTEMA INTELIGENTE FREITULITOS',
+    author: ['logo python.png'],
+  },
+  {
+    img: 'src\\assets\\images\\linktree freitulitos.png',
+    title: 'LinkTree Custom',
+    author: ['logo js.svg', "logo CSS.png", "logo HTML.png"],
+    cols: 2,
+  },
+  {
+    img: 'src\\assets\\images\\site trojan.png',
+    title: 'SITE TROJAN',
+    author: ['logo ts.svg'],
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Contador de TAGS',
+    author: ['logo js.svg'],
+  },
+  {
+    img: 'src\\assets\\images\\APP freitulitos.jpg',
+    title: 'FreitulitosAPP',
+    author: ['logo ts.svg'],
+    cols: 2,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'FirstAPI',
+    author: ['logo react.png'],
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
 ];
+
 const itemData = [
-    {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'PNAD',
-      author: '@bkristastucchio',
-      rows: 2,
-      cols: 2,
-      featured: true,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'IA CALVO',
-      author: '@rollelflex_graphy726',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'SISTEMA INTELIGENTE FREITULITOS',
-      author: '@helloimnik',
-    },
-    {
-      img: 'src\\assets\\images\\painel pam.png',
-      title: 'PAM',
-      author: '@nolanissac',
-      cols: 2,
-    },
-    {
-      img: 'src\\assets\\images\\painel pevs.png',
-      title: 'PEVS',
-      author: '@hjrc33',
-      cols: 2,
-    },
-    {
-      img: 'src\\assets\\images\\painel ppm.png',
-      title: 'PPM',
-      author: '@arwinneil',
-      rows: 2,
-      cols: 2,
-      featured: true,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-      title: 'CONAB',
-      author: '@tjdragotta',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-      title: 'ANP',
-      author: '@tjdragotta',
-    },
-    {
-      img: 'src\\assets\\images\\logo sqllite.png',
-      title: 'USDA',
-      author: '@tjdragotta',
-    },
-    {
-      img: 'src\\assets\\images\\painel pib municipal.png',
-      title: 'PIB MUNICIPAL',
-      author: '@tjdragotta',
-    },
-    {
-      img: 'src\\assets\\images\\logo sqllite.png',
-      title: 'PIM',
-      author: '@tjdragotta',
-    },
-    {
-      img: 'src\\assets\\images\\logo sqllite.png',
-      title: 'PMS',
-      author: '@tjdragotta',
-    },
-    {
-      img: 'src\\assets\\images\\painel abates.png',
-      title: 'ABATE ANIMAIS',
-      author: '@tjdragotta',
-    },
-    {
-      img: 'src\\assets\\images\\logo sqllite.png',
-      title: 'ESTOQUES',
-      author: '@tjdragotta',
-    },
+  {
+    img: 'src\\assets\\images\\painel pnad.png',
+    title: 'PNAD',
+    author: ['logo js.svg'],
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: 'src\\assets\\images\\IA Calvo.png',
+    title: 'IA CALVO',
+    author: ['logo ts.svg'],
+  },
+  {
+    img: 'src\\assets\\images\\painel pam.png',
+    title: 'PAM',
+    author: ['logo react.png'],
+    cols: 2,
+  },
+  {
+    img: 'src\\assets\\images\\painel pevs.png',
+    title: 'PEVS',
+    author: ['logo js.svg'],
+    cols: 2,
+  },
+  {
+    img: 'src\\assets\\images\\painel ppm.png',
+    title: 'PPM',
+    author: ['logo react.png'],
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: 'src\\assets\\images\\painel conab.png',
+    title: 'CONAB',
+    author: ['logo js.svg'],
+  },
+  {
+    img: 'src\\assets\\images\\painel anp.png',
+    title: 'ANP',
+    author: ['logo ts.svg'],
+  },
+  {
+    img: 'src\\assets\\images\\painel pib municipal.png',
+    title: 'PIB MUNICIPAL',
+    author: ['logo react.png'],
+  },
+  {
+    img: 'src\\assets\\images\\painel abates.png',
+    title: 'ABATE ANIMAIS',
+    author: ['logo js.svg'],
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'src\\assets\\images\\painel estoques.png',
+    title: 'ESTOQUES',
+    author: ['logo ts.svg'],
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'src\\assets\\images\\painel pim.png',
+    title: 'PIM',
+    author: ['logo react.png'],
+  },
+  {
+    img: 'src\\assets\\images\\painel pms.png',
+    title: 'PMS',
+    author: ['logo js.svg'],
+  },
+  {
+    img: 'src\\assets\\images\\painel usda.png',
+    title: 'USDA',
+    author: ['logo ts.svg'],
+  },
+  {
+    img: 'src\\assets\\images\\painel ptl.png',
+    title: 'PTL',
+    author: ['logo react.png'],
+  },
 ];
 
 function TabPanel(props: { children?: React.ReactNode; index: number; value: number }) {
@@ -168,11 +178,11 @@ export default function Projects() {
     justifyContent: "center", 
     flexDirection: "column",  
     [theme.breakpoints.up('xs')]: {
-      paddingTop: "100px", // Ajuste para mobile
+      paddingTop: "100px", 
       width: "100%"
     },
     [theme.breakpoints.up('md')]: {
-      paddingTop: "0", // Ajuste para desktop
+      paddingTop: "0", 
     },
   }));
 
@@ -181,27 +191,38 @@ export default function Projects() {
     [theme.breakpoints.down("sm")]: { 
       width: "40px", 
       height: "40px" 
-
-    
     },
     [theme.breakpoints.up("sm")]: { 
       width: "100%",  
       height: "100px", 
     },
-     
   }));
-
   const LogoGallery = ({ logos }: { logos: string[] }) => {
     return (
-      <Box sx={{ display: 'flex', flexDirection: 'row', gap: {xs: '25px', sm: '15px'}, flexWrap: 'wrap', justifyContent: 'center', marginLeft:"25px" }}>
+      <Box sx={{ display: 'flex', flexDirection: 'row', gap: { xs: '5px', sm: '10px' }, marginTop: 0 , padding:0}}>
         {logos.map((logo, index) => (
-          <Box key={index} sx={{gap:"5px"}}> 
-            <StyleImg src={`src/assets/images/${logo}`} alt={`Logo ${index + 1}`} />
+          <Box 
+            key={index} 
+            sx={{ 
+              display: "flex", 
+              alignItems: "center", 
+              justifyContent: "center", 
+              border: "2px solid purple",
+              width: "10%",
+              padding: "1px", // Para não colar a borda na imagem
+            }}
+          >
+            <StyleImg 
+              src={`src/assets/images/${logo}`} 
+              alt={`Logo ${index + 1}`} 
+              sx={{ width: "30px", height: "auto" }} 
+            />
           </Box>
         ))}
       </Box>
     );
   };
+  
   
 
   const StyleBox = styled("div")({
@@ -210,9 +231,7 @@ export default function Projects() {
     height: 'auto',
     width: '100%',
     padding: 0,
-    
   });
-
 
   const StyleIMGList = styled(ImageList)({
     padding: '16px', 
@@ -230,96 +249,92 @@ export default function Projects() {
     <StyleHero>
       <Container maxWidth="lg">
         <Grid2 container spacing={2} sx={{ position: 'relative' }}>
-          <Box sx={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column', 
-            height: 'auto',
-            padding: 0,
-            
-          }}>
+          <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', height: 'auto', padding: 0 }}>
             <StyleBox>
-                <Tabs
-                  orientation="horizontal" 
-                  variant="scrollable"
-                  value={value}
-                  onChange={handleChange}
-                  aria-label="Vertical tabs example"
-                  scrollButtons
-                  allowScrollButtonsMobile
-                  centered
-                  sx={{
-                    borderColor: 'divider',
-                    display: 'flex', 
-                    flexDirection:'column',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    
-                  }}
-                >
-                    <Tab icon={<CodeIcon></CodeIcon>}label="DEV" {...a11yProps(0)} sx={{ fontSize: '20px', fontWeight: 'bold',  }} /> 
-                    <Tab icon={<AddchartIcon></AddchartIcon>} label="DATA SCIENCE" {...a11yProps(1)} sx={{ fontSize: '20px', fontWeight: 'bold',  }} /> 
-                    <Tab icon={<ArchitectureIcon></ArchitectureIcon>} label="DESIGN" {...a11yProps(2)} sx={{ fontSize: '20px', fontWeight: 'bold',  }} /> 
+              <Tabs
+                orientation="horizontal" 
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                scrollButtons
+                allowScrollButtonsMobile
+                centered
+                sx={{
+                  borderColor: 'divider',
+                  display: 'flex', 
+                  flexDirection:'column',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}
+              >
+                <Tab icon={<CodeIcon />} label="DEV" {...a11yProps(0)} sx={{ fontSize: '20px', fontWeight: 'bold' }} /> 
+                <Tab icon={<AddchartIcon />} label="DATA SCIENCE" {...a11yProps(1)} sx={{ fontSize: '20px', fontWeight: 'bold' }} /> 
+                <Tab icon={<ArchitectureIcon />} label="DESIGN" {...a11yProps(2)} sx={{ fontSize: '20px', fontWeight: 'bold' }} /> 
+              </Tabs>
 
-                </Tabs>
-                <TabPanel value={value} index={0}>
-                    <ImageList sx={{ width: "100%", height: 450}}>
-                        <ImageListItem key="Subheader" cols={2}>
-                            <ListSubheader component="div"></ListSubheader>
-                        </ImageListItem>
-                        {itemDev.map((item) => (
-                            <ImageListItem key={item.img} sx={{height:"80%", }}>
-                            <img
-                                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                src={`${item.img}?w=248&fit=crop&auto=format`}
-                                alt={item.title}
-                                loading="lazy"
-                            />
-                            <ImageListItemBar
-                                title={item.title}
-                                subtitle={item.author}
-                                actionIcon={
-                                <IconButton
-                                    sx={{ color: 'rgba(255, 255, 255, 0.54)',  }}
-                                    aria-label={`info about ${item.title}`}
-                                >
-                                    <Icon />
-                                </IconButton>
-                                }
-                            />
-                            </ImageListItem>
-                        ))}
-                    </ImageList>
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    <ImageList sx={{ width: "100%", height: 450,}}>
-                        <ImageListItem key="Subheader" cols={2}>
-                            <ListSubheader component="div"></ListSubheader>
-                        </ImageListItem>
-                        {itemData.map((item) => (
-                            <ImageListItem key={item.img}>
-                            <img
-                                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                src={`${item.img}?w=248&fit=crop&auto=format`}
-                                alt={item.title}
-                                loading="lazy"
-                            />
-                            <ImageListItemBar
-                                title={item.title}
-                                subtitle={item.author}
-                                actionIcon={
-                                <IconButton
-                                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                                    aria-label={`info about ${item.title}`}
-                                >
-                                    <Icon />
-                                </IconButton>
-                                }
-                            />
-                            </ImageListItem>
-                        ))}
-                    </ImageList>
-                </TabPanel>
+              <TabPanel value={value} index={0}>
+                <ImageList sx={{ width: "100%", height: 450, rowHeight: 180 }}>
+                  <ImageListItem key="Subheader" cols={2}>
+                    <ListSubheader component="div"></ListSubheader>
+                  </ImageListItem>
+                  {itemDev.map((item) => (
+                    <ImageListItem key={item.img} sx={{ width: "100%", height: 180 }}>
+                      <img
+                        srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${item.img}?w=248&fit=crop&auto=format`}
+                        alt={item.title}
+                        loading="lazy"
+                        style={{ width: "100%", height: "80%", objectFit: "cover" }}
+                      />
+                      <ImageListItemBar
+                        title={item.title}
+                        subtitle={<LogoGallery logos={item.author} />}
+                        actionIcon={
+                          <IconButton
+                            sx={{ color: 'rgb(255, 255, 255)' }}
+                            aria-label={`info about ${item.title}`}
+                          >
+                            <HelpOutlineIcon />
+                          </IconButton>
+                        }
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
+              </TabPanel>
+
+              <TabPanel value={value} index={1}>
+                <ImageList sx={{ width: "100%", height: 450, rowHeight: 180 }}>
+                  <ImageListItem key="Subheader" cols={2}>
+                    <ListSubheader component="div"></ListSubheader>
+                  </ImageListItem>
+                  {itemData.map((item) => (
+                    <ImageListItem key={item.img} sx={{ width: "100%", height: 180 }}>
+                      <img
+                        srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${item.img}?w=248&fit=crop&auto=format`}
+                        alt={item.title}
+                        loading="lazy"
+                        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                      />
+                      <ImageListItemBar
+                        title={item.title}
+                        subtitle={<LogoGallery logos={item.author} />} // Usando o LogoGallery aqui
+                        actionIcon={
+                          <IconButton
+                            sx={{ color: 'rgb(255, 255, 255)' }}
+                            aria-label={`info about ${item.title}`}
+                          >
+                            <HelpOutlineIcon />
+                          </IconButton>
+                
+                        }
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
+              </TabPanel>
             </StyleBox>
           </Box>
         </Grid2>
