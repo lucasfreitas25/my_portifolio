9718defe6af325e93627840fdd53cb5243a6dbc0
lucasfreitas25 <lucasfreitas25001@gmail.com>
2025-03-10 18:47:14 -0400

    denovo

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my_portifolio",
-  plugins: [react()],
-  publicDir: 'public',
-  build: {
-    outDir: 'dist', // Definindo o diretório de saída para dist
-    assetsDir: "assets", // Definindo o diretório de ativos para assets
-  },
+  base: "/my_portifolio/",
+  plugins: [react()]
 })
