d1a360028f19d76eafcf9ee37f99b3c3fc4f1411
lucasfreitas25 <lucasfreitas25001@gmail.com>
2025-03-24 17:17:29 -0400

    vamos ver

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/my_portifolio/",
-  plugins: [react()]
+  plugins: [react()],
+  base: '/my_portifolio/' 
 })
